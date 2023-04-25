8103192ecfc0dfa5ab0f9e7914b33d5eae1d0ece
Your Name <you@example.com>
2023-04-25 15:11:50 -0300

    iniciando leitura de arquivo json de playlist

--- video-player.js
+++ video-player.js
@@ -1,43 +1,38 @@
-const playlist = document.querySelector("div.playlist")
-
-void async function() {
-  const requisicao = await fetch("list.json")
-  const json = await requisicao.json()
-
-  json.forEach(movie => {
-    playlist.innerHTML += `
-      <div>${movie.title}</div>
-    `
-  })
-}();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// ---✀------------------------------------------------------
+// ENCONTRANDO DOM DOS PLAYERS
+// 
 const containers = document.querySelectorAll("div.ia21-player")
 
-containers.forEach(container => {
+// ---✀------------------------------------------------------
+// PERCORRENDO TODOS OS PLAYERS
+// 
+containers.forEach(async container => {
+  // ---✀------------------------------------------------------
+  // ENCONTRANDO ELEMENTOS DOM DO PLAYER 
+  //
   const btPlayPause = container.querySelector("button.play-pause")
   const video = container.querySelector("video")
   const timeline = container.querySelector(".dragbar.timeline")
   const timelineDrag = timeline.querySelector(".draggable")
   const timer = container.querySelector(".timer")
+  const playlist = container.querySelector(".playlist")
+  const dragbars = container.querySelectorAll(".dragbar")
 
+  // ---✀------------------------------------------------------
+  // LER PLAYLIST DO ARQUIVO JSON
+  //
+  const requisicao = await fetch(container.dataset.playlist)
+  const json = await requisicao.json()
+
+  json.forEach(movie => {
+    playlist.innerHTML += `
+    <div>${movie.title}</div>
+    `
+  })
+
+  // ---✀------------------------------------------------------
+  // BOTÃO PLAY PAUSE
+  //
   btPlayPause.addEventListener("click", () => {
     if (video.paused) {
       video.play()
@@ -49,71 +44,73 @@ containers.forEach(container => {
     btPlayPause.innerText = btPlayPause.dataset.playIcon
   })
 
+  // ---✀------------------------------------------------------
+  // CONTADOR DE TEMPO
+  //
   video.addEventListener("timeupdate", () => {
-    const percent = (video.currentTime / video.duration) * 100 
+    const percent = (video.currentTime / video.duration) * 100
     const s = Math.floor(video.currentTime)
     const m = Math.floor(s / 60)
-    const h = Math.floor(m / 60) 
+    const h = Math.floor(m / 60)
     const sh = `${h % 60}`.padStart(2, "0")
     const sm = `${m % 60}`.padStart(2, "0")
-    const ss = `${s % 60}`.padStart(2, "0")  
+    const ss = `${s % 60}`.padStart(2, "0")
     timelineDrag.style.setProperty("--percent", `${percent}%`)
     timer.innerText = `${sh}:${sm}:${ss}`
   })
 
-// ---✀------------------------------------------------------
-
-const dragbars = container.querySelectorAll(".dragbar")
-
-dragbars.forEach(dragbar => {
-  const dragabble = dragbar.querySelector(".draggable")
-
-  if (dragbar.classList.contains("volume")) {
-    dragabble.style.setProperty("--percent", `100%`)
-  }
-
-  dragbar.addEventListener("mousedown", ev => {
-    dragbar.classList.add("dragging")
-  })
-
-  dragbar.addEventListener("mouseup", ev => {
-    dragbar.classList.remove("dragging")
-  })
-
-  dragbar.addEventListener("mouseout", ev => {
-    dragbar.classList.remove("dragging")
-  })
-
-  dragbar.addEventListener("mousemove", ev => {
-    if (ev.target != dragbar || !dragbar.classList.contains("dragging"))
-      return
-
-    const width = Math.floor(dragbar.getBoundingClientRect().width)
-    const index = (ev.offsetX / width)
-    const percent = index * 100
-
-    dragabble.style.setProperty("--percent", `${percent}%`)
-  })
-
-  dragbar.addEventListener("mouseup", ev => {
-    if (ev.target != dragbar)
-      return
-
-    const width = Math.floor(dragbar.getBoundingClientRect().width)
-    const index = (ev.offsetX / width)
-    const percent = index * 100
-
-    dragabble.style.setProperty("--percent", `${percent}%`)
-
-    if (dragbar.classList.contains("timeline")) {
-      video.currentTime = video.duration * index
-      return
-    }
+  // ---✀------------------------------------------------------
+  // BARRAS (TIMELINE E VOLUME) 
+  //
+  dragbars.forEach(dragbar => {
+    const dragabble = dragbar.querySelector(".draggable")
 
     if (dragbar.classList.contains("volume")) {
-      video.volume = index
-      return
+      dragabble.style.setProperty("--percent", `100%`)
     }
+
+    dragbar.addEventListener("mousedown", ev => {
+      dragbar.classList.add("dragging")
+    })
+
+    dragbar.addEventListener("mouseup", ev => {
+      dragbar.classList.remove("dragging")
+    })
+
+    dragbar.addEventListener("mouseout", ev => {
+      dragbar.classList.remove("dragging")
+    })
+
+    dragbar.addEventListener("mousemove", ev => {
+      if (ev.target != dragbar || !dragbar.classList.contains("dragging"))
+        return
+
+      const width = Math.floor(dragbar.getBoundingClientRect().width)
+      const index = (ev.offsetX / width)
+      const percent = index * 100
+
+      dragabble.style.setProperty("--percent", `${percent}%`)
+    })
+
+    dragbar.addEventListener("mouseup", ev => {
+      if (ev.target != dragbar)
+        return
+
+      const width = Math.floor(dragbar.getBoundingClientRect().width)
+      const index = (ev.offsetX / width)
+      const percent = index * 100
+
+      dragabble.style.setProperty("--percent", `${percent}%`)
+
+      if (dragbar.classList.contains("timeline")) {
+        video.currentTime = video.duration * index
+        return
+      }
+
+      if (dragbar.classList.contains("volume")) {
+        video.volume = index
+        return
+      }
+    })
   })
-})
 })
